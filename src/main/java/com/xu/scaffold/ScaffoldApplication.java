af013c9a46debd20ebecb2c3f62bd18717bfa3df
hua.xu <hua.xu@bitmain.com>
2018-06-05 17:09:49 +0800

    init

---
+++ src/main/java/com/xu/scaffold/ScaffoldApplication.java
@@ -0,0 +1,12 @@
+package com.xu.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaffoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScaffoldApplication.class, args);
+    }
+}
