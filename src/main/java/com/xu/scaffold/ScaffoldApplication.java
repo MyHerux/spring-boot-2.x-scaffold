e536d8a9c4ee6f5ee17d82c04f979477c09a1ff9
hua.xu <hua.xu@bitmain.com>
2018-06-07 16:06:59 +0800

    feature: add redis

--- src/main/java/com/xu/scaffold/ScaffoldApplication.java
+++ src/main/java/com/xu/scaffold/ScaffoldApplication.java
@@ -3,6 +3,7 @@ package com.xu.scaffold;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -11,6 +12,8 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.activation.DataSource;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -39,7 +42,6 @@ public class ScaffoldApplication {
         return new ApiInfoBuilder()
                 .title("RESTful APIs")
                 .description("用于项目前端接口调用")
-                .termsOfServiceUrl("")
                 .version("1.0")
                 .termsOfServiceUrl("11")
                 .build();
--- src/main/java/com/xu/scaffold/controller/TestController.java
+++ src/main/java/com/xu/scaffold/controller/TestController.java
@@ -4,14 +4,21 @@ import com.alibaba.fastjson.JSONObject;
 import com.xu.scaffold.common.bean.ExceptionType;
 import com.xu.scaffold.common.bean.Level;
 import com.xu.scaffold.common.exception.BusinessException;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(description = "TEST")
 @RestController
 public class TestController {
+
+    @Autowired
+    private StringRedisTemplate template;
 
     @GetMapping("/test")
     public String test() {
@@ -41,11 +48,19 @@ public class TestController {
     @PostMapping("/test4")
     public JSONObject test4(@RequestBody JSONObject json) throws BusinessException {
         if (!json.containsKey("data")) {
-            throw new BusinessException(20002,"测试自定义异常，输入数据："+json.toJSONString(),Level.ignore);
+            throw new BusinessException(20002, "测试自定义异常，输入数据：" + json.toJSONString(), Level.ignore);
         }
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("data", "test");
         return jsonObject;
+    }
+
+    @ApiOperation(value = "测试Redis")
+    @GetMapping("/test5")
+    public String test5() {
+        template.opsForValue().set("aaa", "111");
+
+        return template.opsForValue().get("aaa");
     }
 
     private final static String test = "{\"input\":\"test\",\"input2\":\"test2\"}";
